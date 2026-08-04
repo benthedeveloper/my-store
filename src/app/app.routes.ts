467cc1fba9560d22076acb4c7cac6c62a3721f3f
Ben Harrison <benthedeveloper@users.noreply.github.com>
2026-08-04 15:37:31 -0700

    fix: Import RouterLink so header links work as expected. Create new Cart component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Cart } from './cart/cart';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
     title: 'My Store - Home',
+  },
+  {
+    path: 'cart',
+    component: Cart,
+    title: 'My Store - Shopping Cart',
   },
 ];
